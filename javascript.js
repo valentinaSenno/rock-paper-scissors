f05c104f138da09ebd64eb61a7840f14e90cb53a
valentinaSenno <178168883+valentinaSenno@users.noreply.github.com>
2024-09-03 11:14:13 +0200

    Add UI to game

--- javascript.js
+++ javascript.js
@@ -1,3 +1,13 @@
+document.getElementById("rock").addEventListener('click', () => playRound('Rock'));
+document.getElementById("paper").addEventListener('click', () => playRound('Paper'));
+document.getElementById("scissors").addEventListener('click', () => playRound('Scissors'));
+const resultText = document.getElementById("result");
+const humanScoreText = document.getElementById("human-score");
+const computerScoreText = document.getElementById("computer-score");
+
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const computerPlayer = Math.floor(Math.random() * 3) + 1;
     if (computerPlayer === 1) {
@@ -30,40 +40,54 @@ function getHumanChoice() {
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
     humanChoice = getHumanChoice();
     computerChoice = getComputerChoice();
 
+    
       
     if (humanChoice === computerChoice) {
-        console.log(`It's a tie! Both chose ${humanChoice}`);
+        resultText.innerText = `It's a tie! Both chose ${humanChoice}`;
     } else if (
         (humanChoice === 'Rock' && computerChoice === 'Scissors') ||
         (humanChoice === 'Scissors' && computerChoice === 'Paper') ||
         (humanChoice === 'Paper' && computerChoice === 'Rock')
     ) {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        resultText.innerText = `You win! ${humanChoice} beats ${computerChoice}`;
         humanScore++;
     } else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        resultText.innerText = `You lose! ${computerChoice} beats ${humanChoice}`;
         computerScore++;
     }
+    humanScoreText.innerText = humanScore;
+    computerScoreText.innerText = computerScore;
 
+    if (humanScore >= 5) {
+        resultText.innerHTML = 'You win!' + `   <button id="resetScore">Restart game</button>`;
+        document.getElementById("resetScore").addEventListener('click', reset);
+    } else if (computerScore >= 5) {
+        resultText.innerHTML = 'The computer is the winner!' + `   <button id="resetScore">Restart game</button>`;
+        document.getElementById("resetScore").addEventListener('click', reset);
+    }
 }
 
-function playGame() {
+function reset() {
+    humanScore = 0;
+    computerScore = 0;
+    humanScoreText.innerText = '';
+    computerScoreText.innerText = '';
+    resultText.innerHTML = '';
+}
 
-    for (let i = 0; i < 5; i++) {
-        playRound();
+ /* function playGame() {
+
+for (let i = 0; i < 5; i++) {
+           playRound();
     }
     if (humanScore > computerScore) {
-        console.log("You are the winner!");
-    } else {
-        console.log("Try again"); 
-    }
-}
-
-playGame()
+            console.log("You are the winner!");
+         } else {
+               console.log("Try again"); 
+             }
+        }*/ 
